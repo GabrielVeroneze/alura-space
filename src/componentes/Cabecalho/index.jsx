b3d21681941558b68802b723db670592c85bbacd
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-05 21:08:14 -0300

    :sparkles: feat: Importa o módulo css e aplica as classes

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -1,11 +1,15 @@
 import logo from './logo.png'
 import search from './search.png'
+import styles from './Cabecalho.module.sass'
 
 export default function Cabecalho() {
     return (
-        <header>
+        <header className={styles.cabecalho}>
             <img src={logo} alt="Logo do Alura Space" />
-            <input type="text" placeholder="O você procura?" />
+            <div className={styles.cabecalho__campo}>
+                <input className={styles.cabecalho__pesquisa} type="text" placeholder="O você procura?" />
+                <img className={styles.cabecalho__lupa} src={search} alt="Ícone de lupa" />
+            </div>
         </header>
     )
 }
