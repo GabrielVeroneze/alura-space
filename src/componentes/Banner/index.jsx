bc1b408e3eca616cd016731cb362e2eca8dd58de
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-06 22:42:00 -0300

    :sparkles: feat: Adiciona componente de Banner

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Banner.module.sass'
+
+export default function Banner() {
+  return (
+    <section className={styles.banner}>
+        <h1 className={styles.banner__titulo}>A galeria mais completa de fotos do espaço!</h1>
+    </section>
+  )
+}
