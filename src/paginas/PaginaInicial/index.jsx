ad5ad1b00394766fe68ef0e77900fd7bfc968f02
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-06 22:46:37 -0300

    :sparkles: feat: Adiciona conteúdo principal

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,11 +1,16 @@
+import Banner from "../../componentes/Banner";
 import Cabecalho from "../../componentes/Cabecalho";
 import Menu from "../../componentes/Menu";
+import styles from './PaginaInicial.module.sass'
 
 export default function PaginaInicial() {
     return (
         <>
             <Cabecalho />
-            <Menu />
+            <main className={styles.principal}>
+                <Menu />
+                <Banner />
+            </main>
         </>
     )
 }
