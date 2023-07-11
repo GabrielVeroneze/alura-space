66676832cd7ea93fb36df9d2e2c244cb2c6ca323
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-10 23:24:42 -0300

    :sparkles: feat: Adiciona e estiliza lista
    Adiciona e estiliza lista com os cards

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,4 +1,6 @@
+import Cards from "../Cards";
 import Tags from "../Tags";
+import fotos from './fotos.json'
 import styles from './Galeria.module.sass'
 
 export default function Galeria() {
@@ -6,6 +8,11 @@ export default function Galeria() {
         <section className={styles.galeria}>
             <h2 className={styles.galeria__titulo}>Navegue pela galeria</h2>
             <Tags />
+            <ul className={styles.galeria__lista}>
+                {fotos.map(dados => (
+                    <Cards key={dados.id} {...dados} />
+                ))}
+            </ul>
         </section>
     )
 }
