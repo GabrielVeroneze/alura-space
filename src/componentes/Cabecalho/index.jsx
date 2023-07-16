2f716cddabe061873af2d0992ceb85bc7eefb745
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-07-16 18:20:52 -0300

    :sparkles: feat: Adiciona classe para estilização

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -5,9 +5,9 @@ import styles from './Cabecalho.module.sass'
 export default function Cabecalho() {
     return (
         <header className={styles.cabecalho}>
-            <img src={logo} alt="Logo do Alura Space" />
+            <img className={styles.cabecalho__logo} src={logo} alt="Logo do Alura Space" />
             <div className={styles.cabecalho__campo}>
-                <input className={styles.cabecalho__pesquisa} type="text" placeholder="O você procura?" />
+                <input className={styles.cabecalho__pesquisa} type="text" placeholder="O que você procura?" />
                 <img className={styles.cabecalho__lupa} src={search} alt="Ícone de lupa" />
             </div>
         </header>
